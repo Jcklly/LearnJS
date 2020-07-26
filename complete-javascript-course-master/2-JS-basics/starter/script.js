c19398e87f70681231b3c485c99d7eafb733cebf
Jack Kelly <jack@Jacks-MacBook-Pro.local>
2020-07-26 19:02:57 -0400

    Modified

---
+++ complete-javascript-course-master/2-JS-basics/starter/script.js
@@ -0,0 +1,16 @@
+/*
+var firstName = "Jack";
+var lastName = "Kelly";
+var age = 28;
+
+console.log(firstName + " " + lastName + " " + age);
+*/
+
+/*********************************
+ * Variable name and type coercion
+ */
+
+var age = 22;
+var fullName = "Jack Kelly";
+
+console.log(fullName + " " + age);
